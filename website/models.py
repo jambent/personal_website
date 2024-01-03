1ff2efce0bfd93cbcc597509334d1e879e7f8b69
jwbennett <jwbennett@Laptop>
2024-01-03 11:08:56 +0000

    Adds basic project structure

---
+++ website/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField("Category", related_name="posts")
+
+    def __str__(self):
+        return self.title
