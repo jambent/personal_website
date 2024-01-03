b64cacfffc16a573fb85c9ceb437198c107ca2da
jwbennett <jwbennett@Laptop>
2024-01-03 17:25:36 +0000

    Adds Python script run-via-button capability (but ruins other elements on page)

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls import path,re_path
+from . import views, python_script
 
 urlpatterns = [
     path("", views.website_index, name="website_index"),
     path("post/<int:pk>/", views.website_detail, name="website_detail"),
     path("category/<category>/", views.website_category, name="website_category"),
+    re_path(r'^$', python_script.button),
+    re_path(r'^output', python_script.output,name="script")
+
 ]
